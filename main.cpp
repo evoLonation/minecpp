fec1aa2ed27891a3cbe7b41186fa55f7220032e8
evoLonation <1838940019@qq.com>
2023-05-26 09:33:42 +0800

    import libfmt

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "example/2d_transform.hpp"
 #include "render/render.hpp"
+#include <fmt/format.h>
 
 int main(){
    if(transform2d::run()){
       printf("success run\n");
    }
-   return 0;
+   using namespace std;
+   cout<<fmt::format("{}\n", 1); // format，输出完整的3.14159265358979
+   return 0;   
 }
