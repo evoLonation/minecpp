e38222299d8c718e585cf3aab8dab02b05f77013
evoLonation <1838940019@qq.com>
2023-06-08 21:28:14 +0800

    add observable and ref observable

--- src/tool.hpp
+++ src/tool.hpp
@@ -26,16 +26,16 @@ public:
 inline std::vector<std::function<void(void)>*> chainCall;
 inline std::set<std::function<void(void)>*> chainCallSet;
 
-// 用于存储数据是否“脏”
-// 每个observable可以产生多个关联的 observer
-// 如果在一个调用链中出现了重复的 observer 就会直接结束调用
-template<typename T>
-class DirtyObservable{
+// 可观察对象
+class Observable{
 private:
-   T mValue;
    std::vector<std::function<void(void)>> observers;
-   void publish() noexcept{
-      
+protected:
+   // abstract class
+   Observable() noexcept = default;
+
+   // 通知观察者，会防止循环调用 
+   void notice(){
       for(auto& observer : observers){
          // 如果调用链中包含此对象，说明发生了循环调用，直接返回
          if(chainCallSet.contains(&observer)){
@@ -52,25 +52,53 @@ private:
       }
    }
 public:
+   void addObserver(const std::function<void(void)>& observer)noexcept{
+      observers.push_back(observer);
+   }
+};
+
+// 将对于观察者的通知封装到对数据的赋值操作中
+template<typename T>
+class DirtyObservable: public Observable{
+private:
+   T mValue;
+public:
+   DirtyObservable(const T&& t)noexcept:mValue(t){}
    DirtyObservable(T&& t)noexcept:mValue(std::move(t)){}
    
    DirtyObservable& operator=(const T& t)noexcept{
-      
       mValue = t;
-      publish();
+      notice();
       return *this;
    }
    DirtyObservable& operator=(T&& t)noexcept{
       mValue = std::move(t);
-      publish();
+      notice();
       return *this;
    }
-   const T& value()noexcept{ return mValue; }
-   
-   void addObserver(const std::function<void(void)>& observer)noexcept{
-      observers.push_back(observer);
-   }
 
+   const T& value()noexcept{ return mValue; }
+};
+
+// 封装数据，当调用check时检查数据距离初始化或者上次check时是否变化，如果变化则通知观察者
+template<typename T>
+class RefDirtyObservable: public Observable{
+private:
+   T mValue;
+   T oldValue;
+public:
+   // same as dirtyobserable
+   RefDirtyObservable(const T&& t)noexcept:mValue(t), oldValue(t){}
+   RefDirtyObservable(T&& t)noexcept:mValue(std::move(t)), oldValue(mValue){}
+   
+   T& value()noexcept{return mValue;}
+
+   void check(){
+      if(oldValue != mValue){
+         oldValue = mValue;
+         notice();
+      }
+   }
 };
 
 } // namespace minecpp
