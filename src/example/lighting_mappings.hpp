d05208d22ab1e76115067f0ee29b20194e425b4d
evoLonation <1838940019@qq.com>
2023-06-09 21:02:43 +0800

    改动后的重构

--- src/example/lighting_mappings.hpp
+++ src/example/lighting_mappings.hpp
@@ -2,6 +2,7 @@
 #define _MINECPP_LIGHTING_MAPPINGS_H_
 #include <gl.h>
 #include "../resource.hpp"
+#include "../imgui.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -60,44 +61,13 @@ float vertices[] = {
    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
 };
 
-void init(Context& ctx){
-   // imgui 准备阶段 : 
-   // call ImGui::CreateContext()
-   // call ImGui_ImplXXXX_Init() for each backend
-   ImGui::CreateContext();
-   // install_callbacks: 如果为true，则imgui会设置glfw的相关callback, 原来的callback（如果有）的函数指针会保存，imgui的callback会先调用
-   ImGui_ImplGlfw_InitForOpenGL(ctx.getWindow(), true);
-   std::string glslVersion = fmt::format("#version {}{}0", ctx.majorVersion, ctx.minorVersion);
-   ImGui_ImplOpenGL3_Init(glslVersion.c_str());
-}
-void newFrame(){
-   // frame 创建阶段: 
-   // call ImGui_ImplXXXX_NewFrame() for each backend
-   // call ImGui::NewFrame() 
-   ImGui_ImplOpenGL3_NewFrame();
-   ImGui_ImplGlfw_NewFrame();
-   ImGui::NewFrame();
-}
-void render(){
-   // render 阶段:  call ImGui::Render() and call ImGui_ImplXXXX_RenderDrawData() for Renderer backend
-   ImGui::Render();
-   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-void destroy(){
-   // 销毁阶段: 
-   // call ImGui_ImplXXXX_Shutdown() for each backend 
-   // call ImGui::DestroyContext()
-   ImGui_ImplGlfw_Shutdown();
-   ImGui_ImplOpenGL3_Shutdown();
-   ImGui::DestroyContext();
-}
-
 int run(){
    try{
-      auto& ctx = Context::getInstance();
-      ctx.createWindow(800, 600);
+      Context ctx {800, 600};
       auto& width = ctx.getWidth();
       auto& height = ctx.getHeight();
+      InputProcessor processor;
+      Drawer drawer;
       Program cubeProgram {
          VertexShader::fromFile("../shader/lightingmapping/cube.vertex.glsl"),
          FragmentShader::fromFile("../shader/lightingmapping/cube.frag.glsl")
@@ -148,8 +118,8 @@ int run(){
       DirtyObservable lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
 
 
-      DrawUnit cube  {vao, cubeProgram};
-      DrawUnit light {vao, lightProgram};
+      DrawUnit cube  {vao, cubeProgram, GL_TRIANGLES, 64};
+      DrawUnit light {vao, lightProgram, GL_TRIANGLES, 64};
 
       // cube: material
       auto shininess = 64.0f;
@@ -185,39 +155,21 @@ int run(){
 
       light.addUniform("color", lightColor.value());
 
-      init(ctx);
-      while (!glfwWindowShouldClose(ctx.getWindow()))
-      {
-         
-         // 设置清除缓冲区的颜色并清除缓冲区
-         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-         // 同时清除颜色缓冲区和深度缓冲区
-         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-         cube.draw();
-         glDrawArrays(GL_TRIANGLES, 0, 64);
-         light.draw();
-         glDrawArrays(GL_TRIANGLES, 0, 64);
-
+      drawer.addDrawUnit(cube);
+      drawer.addDrawUnit(light);
+      GuiContext guiCtx;
+      ctx.startLoop([&]{
+         GuiFrame guiFrame;
          auto& lightPosVal = lightPos.value();
-         newFrame();
          ImGui::Begin("controller");
          ImGui::SliderFloat("light position: x", &lightPosVal.x, -5.0f, 5.0f);
          ImGui::SliderFloat("light position: y", &lightPosVal.y, -5.0f, 5.0f);
          ImGui::SliderFloat("light position: z", &lightPosVal.z, -5.0f, 5.0f);
          ImGui::End();
-         render();
          lightPos.check();
-
-         // lightPos = glm::rotate(lightPos.value(), glm::radians(0.02f), glm::vec3(0.0f, 1.0f, 0.0f));
-         // lightColor = glm::vec3(sin(glfwGetTime() * 2.0f), sin(glfwGetTime() * 0.7f), sin(glfwGetTime() * 1.3f));
-
-         glfwSwapBuffers(ctx.getWindow());
-         glfwPollEvents();
-         checkGLError();
-      }
-      destroy();
-      
+         drawer.draw([&]{guiFrame.render();});
+         processor.processInput();
+      });
 
    }catch(std::string e){
       fmt::println("{}", e);
